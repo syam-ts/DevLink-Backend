e4ee26769906b3d02066922583c98d7c21d7a905
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-01-25 21:44:31 +0530

    refactor: verify user validaion done

--- src/application/usecases/user/editProfile.ts
+++ src/application/usecases/user/editProfile.ts
@@ -3,184 +3,198 @@ export interface UserRepositary {
 }
 
 export class EditUserProfile {
-  constructor(private userRepositary: UserRepositary) {}
+  constructor(private userRepositary: UserRepositary) { }
 
   async execute(userId: string, profileData: any, type: string) {
 
-        if(type === 'verify') {
-          
+    if (type === 'verify') {
+
 
       const { name, budget, location, mobile, skills, profilePicture, domain, githubLink, description, whyHireMe, experience, education } = profileData.editData;
 
-
-      
-
-          if(!name || !budget || !location || !mobile || !skills || !profilePicture || !domain || !githubLink || !description || !whyHireMe || !experience || !education ) {
-                throw new Error('All fields need to be filled')
-                 }
-          if (profileData.editData.name) {
-            if (
-              profileData.editData.name.length <= 3 ||
-              profileData.editData.name.length > 20
-            ) {
-              throw new Error("Name should contain atlest 4 characters");
-            }
-          }
-      
-          if (profileData.editData.age) {
-            if (profileData.editData.age < 18 || profileData.editData.age > 70) {
-              if (profileData.editData.age < 18) {
-                throw new Error("Only adult can verify");
-              } else if (profileData.editData.age > 70) {
-                throw new Error("You need to be under 70");
-              } else {
-                throw new Error("Age need to be valid");
-              }
-            }
-          }
-      
-          if (profileData.editData.mobile) {
-            if (profileData.editData.mobile.toString().length < 10) {
-              throw new Error("Number should be atlest 10 digits");
-            }
-          }
-      
-          if (profileData.editData.location) {
-            if (profileData.editData.location.length < 3) {
-              throw new Error("Location need to valid");
-            }
-          }
-      
-          //TODO
-          // if(typeof(profilePicture) === 'png | svg | jpeg | jpg') {
-          //     throw new Error('Invalid Profile picture')
-          // }
-      
-          if (profileData.editData.description) {
-            if (profileData.editData.description.length < 15) {
-              throw new Error("Description should atlest need 15 words");
-            }
-          }
-      
-          if (!profileData.editData.skills) {
-            if (profileData.editData.skills.length < 2) {
-              throw new Error("Add atlest 2 skills");
-            }
-          }
-      
-          if (profileData.editData.budget) {
-            if (
-              profileData.editData.budget < 100 ||
-              profileData.editData.budget > 2000
-            ) {
-              if (profileData.editData.budget < 100) {
-                throw new Error("Pay per Hour need to be atlest 100₹");
-              } else if (profileData.editData.budget > 2000) {
-                throw new Error("Maximum pay per hour is 2000₹");
-              }
-            }
-          }
-      
-        
-      
-          const updatedUser = await this.userRepositary.editUserProfile(
-            userId,
-            profileData
-          );
-          if (!updatedUser) {
-            throw new Error("Profile editing failed");
-          }
-      
-          
-      
-          return { updatedUser };
+      if (!name || !budget || !location || !mobile || !skills || !profilePicture || !domain || !githubLink || !description || !whyHireMe || !experience || !education) {
+        throw new Error('All fields need to be filled')
+      };
 
 
+     
+        if ( name.length <= 3 || name.length > 20) {
+          throw new Error("Name should contain atlest 4 characters");
+        }
+      
+  
+ 
 
-        } else if (type === 'edit') {
-
+     
+        if (mobile.toString().length < 10) {
+          throw new Error("Number should be atlest 10 digits");
+        }
+ 
+           
+     
+        if (location.length < 3) {
+          throw new Error("Location need to valid");
         }
 
-
-    if (profileData.editData.name) {
-      if (
-        profileData.editData.name.length <= 3 ||
-        profileData.editData.name.length > 20
-      ) {
-        throw new Error("Name should contain atlest 4 characters");
-      }
-    }
-
-    if (profileData.editData.age) {
-      if (profileData.editData.age < 18 || profileData.editData.age > 70) {
-        if (profileData.editData.age < 18) {
-          throw new Error("Only adult can verify");
-        } else if (profileData.editData.age > 70) {
-          throw new Error("You need to be under 70");
-        } else {
-          throw new Error("Age need to be valid");
+     
+        if (skills.length < 3) {
+          throw new Error("Require atleast 3 skills");
         }
-      }
-    }
-
-    if (profileData.editData.mobile) {
-      if (profileData.editData.mobile.toString().length < 10) {
-        throw new Error("Number should be atlest 10 digits");
-      }
-    }
-
-    if (profileData.editData.location) {
-      if (profileData.editData.location.length < 3) {
-        throw new Error("Location need to valid");
-      }
-    }
-
-    //TODO
-    // if(typeof(profilePicture) === 'png | svg | jpeg | jpg') {
-    //     throw new Error('Invalid Profile picture')
-    // }
-
-    if (profileData.editData.description) {
-      if (profileData.editData.description.length < 15) {
-        throw new Error("Description should atlest need 15 words");
-      }
-    }
-
-    if (!profileData.editData.skills) {
-      if (profileData.editData.skills.length < 2) {
-        throw new Error("Add atlest 2 skills");
-      }
-    }
-
-    if (profileData.editData.budget) {
-      if (
-        profileData.editData.budget < 100 ||
-        profileData.editData.budget > 2000
-      ) {
-        if (profileData.editData.budget < 100) {
-          throw new Error("Pay per Hour need to be atlest 100₹");
-        } else if (profileData.editData.budget > 2000) {
-          throw new Error("Maximum pay per hour is 2000₹");
+       
+     
+        if (domain.length < 4) {
+          throw new Error("Enter valid domain");
         }
-      }
-    }
+       
+       
+     
+        if (githubLink.length < 20) {
+          throw new Error("Enter valid github link");
+        }
+       
 
-    for (let value in profileData.editData) {
-      if (profileData.editData[value] == "") {
-        profileData.editData[value] = profileData.unchangedData[value];
-      }
-    }
+      //TODO
+      // if(typeof(profilePicture) === 'png | svg | jpeg | jpg') {
+      //     throw new Error('Invalid Profile picture')
+      // }
 
-    const updatedUser = await this.userRepositary.editUserProfile(
-      userId,
-      profileData
-    );
-    if (!updatedUser) {
-      throw new Error("Profile editing failed");
-    }
-
+     
+        if (description.length < 20) {
+          throw new Error("Description should atlest need 20 words");
+        }
+     
+     
+        if (whyHireMe.length < 20) {
+          throw new Error("Hire me field need to have atlest need 20 words");
+        }
+    
+     
+        if (experience.length < 20) {
+          throw new Error("Experince field need to have atlest need 20 words");
+        }
+    
+ 
+     
+        if (education.length < 20) {
+          throw new Error("Education field need to have atlest need 20 words");
+        }
     
 
-    return { updatedUser };
+    
+ 
+        if (
+          budget < 100 ||
+          budget > 2000
+        ) {
+          if (budget < 100) {
+            throw new Error("Pay per Hour need to be atlest 100₹");
+          } else if (budget > 2000) {
+            throw new Error("Maximum pay per hour is 2000₹");
+          }
+        }
+  
+
+
+
+      const updatedUser = await this.userRepositary.editUserProfile(
+        userId,
+        profileData
+      );
+      if (!updatedUser) {
+        throw new Error("Profile editing failed");
+      }
+
+
+
+      return { updatedUser };
+
+
+
+    } else if (type === 'edit') {
+
+
+
+
+      if (profileData.editData.name) {
+        if (
+          profileData.editData.name.length <= 3 ||
+          profileData.editData.name.length > 20
+        ) {
+          throw new Error("Name should contain atlest 4 characters");
+        }
+      }
+
+      if (profileData.editData.age) {
+        if (profileData.editData.age < 18 || profileData.editData.age > 70) {
+          if (profileData.editData.age < 18) {
+            throw new Error("Only adult can verify");
+          } else if (profileData.editData.age > 70) {
+            throw new Error("You need to be under 70");
+          } else {
+            throw new Error("Age need to be valid");
+          }
+        }
+      }
+
+      if (profileData.editData.mobile) {
+        if (profileData.editData.mobile.toString().length < 10) {
+          throw new Error("Number should be atlest 10 digits");
+        }
+      }
+
+      if (profileData.editData.location) {
+        if (profileData.editData.location.length < 3) {
+          throw new Error("Location need to valid");
+        }
+      }
+
+      //TODO
+      // if(typeof(profilePicture) === 'png | svg | jpeg | jpg') {
+      //     throw new Error('Invalid Profile picture')
+      // }
+
+      if (profileData.editData.description) {
+        if (profileData.editData.description.length < 15) {
+          throw new Error("Description should atlest need 15 words");
+        }
+      }
+
+      if (!profileData.editData.skills) {
+        if (profileData.editData.skills.length < 2) {
+          throw new Error("Add atlest 2 skills");
+        }
+      }
+
+      if (profileData.editData.budget) {
+        if (
+          profileData.editData.budget < 100 ||
+          profileData.editData.budget > 2000
+        ) {
+          if (profileData.editData.budget < 100) {
+            throw new Error("Pay per Hour need to be atlest 100₹");
+          } else if (profileData.editData.budget > 2000) {
+            throw new Error("Maximum pay per hour is 2000₹");
+          }
+        }
+      }
+
+      for (let value in profileData.editData) {
+        if (profileData.editData[value] == "") {
+          profileData.editData[value] = profileData.unchangedData[value];
+        }
+      }
+
+      const updatedUser = await this.userRepositary.editUserProfile(
+        userId,
+        profileData
+      );
+      if (!updatedUser) {
+        throw new Error("Profile editing failed");
+      }
+
+
+
+      return { updatedUser };
+    }
   }
 }
