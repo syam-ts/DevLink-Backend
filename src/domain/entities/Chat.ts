d3c05b5b2f556e35ce25c22da67e0ae362b71da8
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-01-27 11:58:13 +0530

    fix: api bug fix message entity

--- src/domain/entities/Chat.ts
+++ src/domain/entities/Chat.ts
@@ -1,19 +1,33 @@
 import mongoose, { Document, Schema, model } from 'mongoose';
 
-export interface Chat extends Document{
-    members: [ mongoose.Schema.Types.ObjectId ],
+
+export interface Chat extends Document {
+    members: 
+        {
+            clientId: mongoose.Types.ObjectId,
+            clientName: String
+       
+            userId: mongoose.Types.ObjectId,
+            userName: String
+        },
+       
+     
     createdAt: Date
-}
+};
 
 
-const ChatSchema: Schema = new Schema({ 
- 
-    members: {
-        type: [
-            {type: mongoose.Types.ObjectId}
-        ]
-    },
-    createdAt: {type: Date, required: true}
+const ChatSchema: Schema = new Schema({
+
+    members:  
+            {
+                clientId: { type: mongoose.Types.ObjectId },
+                clientName: { type: String },
+          
+                userId: { type: mongoose.Types.ObjectId , required: true},
+                userName: { type: String }
+            },
+        
+    createdAt: { type: Date, required: true }
 });
 
 
--- src/domain/interfaces/Repositaries/ChatRepository.ts
+++ src/domain/interfaces/Repositaries/ChatRepository.ts
@@ -1,65 +1,70 @@
-import mongoose from 'mongoose';
-import { ChatModel } from '../../../domain/entities/Chat';
-import { MessageModel } from '../../entities/Message';
-
+import mongoose from "mongoose";
+import { ChatModel } from "../../../domain/entities/Chat";
+import { MessageModel } from "../../entities/Message";
+import { UserModel } from "../../entities/User";
+import { ClientModel } from "../../entities/Client";
 
 interface Members {
-  members: string[]
+  members: string[];
 }
 
-export interface Message {
+interface Message {
   chatId: string;
   sender: string;
   text: string;
   read: boolean;
 }
 
-
-
-
 export class ChatRepositoryMongoose {
 
   async createChat(membersIds: any): Promise<any> {
+    const clientName: any = await ClientModel.findById(
+      membersIds.members.clientId
+    );
+    const userName: any = await UserModel.findById(membersIds.members.userId);
 
+    const members = {
+      clientId: membersIds.members.clientId,
+      clientName: clientName.companyName,
+      userId: membersIds.members.userId,
+      userName: userName.name,
+    };
 
     const updateChat = new ChatModel({
-      members: membersIds.members.map((id: any) => new mongoose.Types.ObjectId(id)),
-      createdAt: new Date()
+      members,
+      createdAt: new Date(),
     });
 
     const savedChat = await updateChat.save();
+    console.log("saved", savedChat);
 
     return savedChat;
   }
 
 
+
   async sendMessage(body: Message): Promise<any> {
-  
     const { chatId, sender, text }: Message = body;
 
     const newMessage = new MessageModel({
       chatId: chatId,
       sender: sender,
-      text: text
+      text: text,
     });
 
     const savedMessage = await newMessage.save();
-
-
-
 
     return savedMessage;
   }
 
 
-  async getAllChats(memberId: string): Promise<any> {
-  
-     const allChats = await ChatModel.find({members: {$in: memberId}});
 
-     if(!allChats) throw new Error('No chats found');
-     console.log('THE RESPO : ',allChats)
+  async getAllChats(memberId: string): Promise<any> {
+    const allChats = await ChatModel.find({ $or:[{"members.clientId": memberId}, {"members.userId": memberId}] });
+ 
+
+    if (!allChats) throw new Error("No chats found");
 
     return allChats;
   }
-
 }
