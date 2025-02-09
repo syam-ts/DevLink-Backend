f5a5d9ab70dda6b3507ca816cc175e688208f154
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-02-09 17:29:12 +0530

    fix: db connection string fix

--- src/infrastructure/database/db.ts
+++ src/infrastructure/database/db.ts
@@ -2,15 +2,15 @@ import mongoose from 'mongoose';
 require('dotenv').config();
 
  
-const mongoUri: string | undefined  = process.env.MONGO_URI;
+const MONGO_URI: string  = process.env.MONGO_URI as string;
 
 export const connectDB = async () => {
     try {
-        mongoUri === undefined ? 
-      console.error('environment variable is undefined') :
-      await mongoose.connect("mongodb+srv://syamnandhu3:AUZcKAsIJHM5phLC@cluster0.ukj87.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");  
+        
+      await mongoose.connect(MONGO_URI);  
         console.log('Database Connection established');
     } catch (err: any) {
         console.error('Failed to Connect Database', err);
     }
-}
+};
+
