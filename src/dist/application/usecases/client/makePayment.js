4c377e693a72efb926200288f9bbb48d1c11d093
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-03-30 23:31:20 +0530

    fix: removed .env

--- src/dist/application/usecases/client/makePayment.js
+++ src/dist/application/usecases/client/makePayment.js
@@ -14,6 +14,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MakePayment = void 0;
 const stripe_1 = __importDefault(require("stripe"));
+
+
 console.log("The port: ", process.env.PORT);
 console.log("The stripe key: ", process.env.STRIPE_SECRET_KEY);
 const stripe = new stripe_1.default(process.env.STRIPE_SECRET_KEY);
