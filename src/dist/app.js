61f87d16dc57767230e448e64e4b729b31df7a34
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-03-31 14:03:04 +0530

    fix: add alternative frontend url

--- src/dist/app.js
+++ src/dist/app.js
@@ -25,7 +25,7 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: process.env.FRONTEND_ORIGIN,
+    origin: process.env.FRONTEND_ORIGIN || 'http://localhost:5173',
     credentials: true,
 }));
 app.use((0, morgan_1.default)("dev"));
