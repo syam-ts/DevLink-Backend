66c6b7e882136341675919fa424e8a9d039a440f
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-04-08 21:28:58 +0530

    refactor: modified logger.ts for non production environment

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -4,6 +4,13 @@ let logger: any;
 
 if (process.env.NODE_ENV === "production") {
     logger = devlinkLogger();
+} else {
+    logger = {
+        info: (...args: any[]) => console.log("[INFO]", ...args),
+        error: (...args: any[]) => console.error("[ERROR]", ...args),
+        warn: (...args: any[]) => console.warn("[WARN]", ...args),
+        debug: (...args: any[]) => console.debug("[DEBUG]", ...args),
+    };
 }
 
 export default logger;
