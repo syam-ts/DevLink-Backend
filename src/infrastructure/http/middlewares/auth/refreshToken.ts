c2653987cdb59865187efe72732d452ad43ac565
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-04-09 22:08:32 +0530

    fix: refresh token return bug fix

--- src/infrastructure/http/middlewares/auth/refreshToken.ts
+++ src/infrastructure/http/middlewares/auth/refreshToken.ts
@@ -5,28 +5,29 @@ import { Request, Response } from "express";
 interface User {
     _id: string
     role: string
-  };
+};
 
 const refreshToken = (req: Request, res: Response): void => {
-    const refreshToken = req.cookies.refreshToken; 
+    const refreshToken = req.cookies.refreshToken;
 
-    if (!refreshToken)
-          res.status(401).json({ message: "No refresh token" });
+    if (!refreshToken) {
+        res.status(401).json({ message: "No refresh token" });
+        return;
+    }
 
     jwt.verify(
         refreshToken,
         process.env.REFRESH_TOKEN_SECRET as string,
         (err: jwt.VerifyErrors | null, decoded: string | JwtPayload | undefined) => {
-            if (err)
-                return res
-                    .status(403)
-                    .json({ message: "Invalid or expired refresh token" });
+            if (err || !decoded) {
+                res.status(403).json({ message: "Invalid or expired refresh token" });
+                return;
+            }
 
             const { accessToken } = generateTokens(decoded as User);
             res.json({ accessToken });
         }
     );
-    return;
 };
 
 export default refreshToken;
