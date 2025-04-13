2aab2b9c9b03e37ac8c68cc084fa945cf4fabc03
| ｓｙａｍ | <syamnandhu3@gmail.com>
2025-04-13 15:04:43 +0530

    fix: modified .env in chatbot

--- src/application/usecases/user/ChatBot.ts
+++ src/application/usecases/user/ChatBot.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config({
   path: path.resolve(__dirname, '.env')
 });
+console.log('ENV FILE LOADS HERE: ',process.env.GROQ_API_KEY)
 
 const groq = new Grog({
   apiKey: process.env.GROQ_API_KEY,
