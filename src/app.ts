7a412701ef3d44bcd48ff0749c58a616f3d93f47
syam-ts <syamnandhu3@gmail.com>
2025-07-21 21:08:56 +0530

    refactor: modified server

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,5 @@
-import express, { NextFunction, Request, Response } from "express";
-import path from 'node:path';
-import dotenv from 'dotenv';
-dotenv.config({
-  path: path.resolve(__dirname, '.env')
-});
-import logger from './logger/logger'
-import cors from "cors";
-import cookieparser from "cookie-parser";
-import http from "node:http";
-import morgan from "morgan";
-import routes from "./infrastructure/http/routes";
-import { connectDB } from "./infrastructure/database/db";
-import initializeSocket from "./infrastructure/socket/socket";
+import Server from './server';
 
-const app = express();
-//pushing all logs to info
-app.use((req: Request, res: Response, next: NextFunction) => {  
-    logger.info(`route: ${req.url}`); 
-  next();
-}); 
+const server = new Server();
 
-app.use(express.json());
-app.use(cookieparser());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_ORIGIN || 'https://dev-link-frontend.vercel.app',
-    credentials: true,
-  })
-);
-
-app.use(morgan("dev")); 
-app.use("/", routes);
-
-const server = http.createServer(app); 
-initializeSocket(server);
-const PORT = (process.env.PORT as string) || 3000;
-
-(async () => {
-  await connectDB();
-  server.listen(PORT, () =>  console.log(`Server running on port ${PORT}`))
-})();
+server.start();
