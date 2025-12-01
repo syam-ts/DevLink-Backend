6afdb1a82854bf279c872921b3baef6410632187
unknown <syamnandhu3@gmail.com>
2025-12-01 21:03:05 +0530

    fix: chatbot fix

--- src/application/usecases/user/ChatBot.ts
+++ src/application/usecases/user/ChatBot.ts
@@ -1,39 +1,40 @@
-import Grog from "groq-sdk";
-import path from "node:path";
-import dotenv from "dotenv";
-dotenv.config({
-  path: path.resolve(__dirname, '../../../.env')
-});
- 
-console.log('THE NEW API KEY FOR GROQ TEST: ', process.env.GROQ_API_KEY);
+// import Grog from "groq-sdk";
+// import path from "node:path";
+// import dotenv from "dotenv";
+// dotenv.config({
+//   path: path.resolve(__dirname, '../../../.env')
+// }); 
 
-const groq = new Grog({
-  apiKey: process.env.GROQ_API_KEY as string,
-});
+// const groq = new Grog({
+//   apiKey: process.env.GROQ_API_KEY as string,
+// });
 
 export class ChatBot {
   async execute(userInput: string) {
     try {
       const chatComletion = await getResult(userInput);
 
-      const data = chatComletion.choices[0]?.message?.content;
+      // const data = chatComletion.choices[0]?.message?.content;
 
-      return data;
+      // return data;
+      return;
     } catch (error) {
-      console.error("Chatbot API Error:", error);
-      return error;
+      return;
+      // console.error("Chatbot API Error:", error);
+      // return error;
     }
   }
 }
 
 async function getResult(input: string) {
-  return groq.chat.completions.create({
-    messages: [
-      {
-        role: "user",
-        content: input,
-      },
-    ],
-    model: "llama-3.1-8b-instant",
-  });
+  return
+  // return groq.chat.completions.create({
+  //   messages: [
+  //     {
+  //       role: "user",
+  //       content: input,
+  //     },
+  //   ],
+  //   model: "llama-3.1-8b-instant",
+  // });
 }
